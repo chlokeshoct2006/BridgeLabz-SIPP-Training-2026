652fe7bb6369e1fd35d110f0014432c434f2b6ba
Lokesh Chaudhary <chlokeshoct2006@gmail.com>
2026-06-23 13:47:30 +0530

    oops folder structure

--- core-java-practice/gcr-codebase/programming-elements/Level 1/SamAverage.java
+++
@@ -1,8 +0,0 @@
-public class SamAverage {
-    public static void main(String[] args) {
-        String name = "Sam";
-        double maths = 94, physics = 95, chemistry = 96;
-        double average = (maths + physics + chemistry) / 3;
-        System.out.println(name + "'s average mark in PCM is " + average);
-    }
-}
